a53ec7a3b3b95f4c3fa3512d8f242553081296ab
Dhruv ;-) <dhruvthummar2210@gmail.com>
2022-07-17 11:08:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+// let country = document.querySelector("#country");
+let city = document.querySelector("#city");
+let check = document.querySelector("#check");
+let tempIcon = document.querySelector("#tempIcon");
+//let weatherCountry = document.querySelector("#weatherCountry");
+let temperature = document.querySelector("#temperature");
+let weatherDescription = document.querySelector("#weatherDescription");
+let feelsLike = document.querySelector("#feelsLike");
+let humidity = document.querySelector("#humidity");
+let longitude = document.querySelector("#longitude");
+let latitude = document.querySelector("#latitude");
+
+check.addEventListener("click", () => {
+    let key = `bd4ea33ecf905116d12af172e008dbae`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&units=metric&appid=${key}`;
+
+    fetch(url).then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+        weatherCountry.innerText = `${data.name} / ${data.sys.country}`;
+        temperature.innerHTML = `${data.main.temp}°<b>C</b>`;
+        //document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + "')";
+
+        data.weather.forEach(items => {
+            weatherDescription.innerText = items.description;
+            if (items.id < 250) {
+                tempIcon.src = `tempicons/storm.svg`;
+            } else if (items.id < 350) {
+                tempIcon.src = `tempicons/drizzle.svg`;
+            } else if (items.id < 550) {
+                tempIcon.src = `tempicons/snow.svg`;
+            } else if (items.id < 650) {
+                tempIcon.src = `tempicons/rain.svg`;
+            } else if (items.id < 800) {
+                tempIcon.src = `tempicons/atmosphere.svg`;
+            } else if (items.id === 800) {
+                tempIcon.src = `tempicons/sun.svg`;
+            } else if (items.id > 800) {
+                tempIcon.src = `tempicons/clouds.svg`;
+            }
+        })
+
+        feelsLike.innerText = `Feels Like ${data.main.feels_like}°C`;
+        
+        humidity.innerText = `Humidity ${data.main.humidity}`;
+        latitude.innerText = `Latitude ${data.coord.lat}`;
+        longitude.innerText = `Latitude ${data.coord.lon}`;
+        
+    })
+    //country.value = "";
+    city.value = "";
+})
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: { lat: 40.731, lng: -73.997 },
+    });
+    const geocoder = new google.maps.Geocoder();
+    const infowindow = new google.maps.InfoWindow();
+  
+    document.getElementById("submit").addEventListener("click", () => {
+      geocodeLatLng(geocoder, map, infowindow);
+    });
+  }
+  
+  function geocodeLatLng(geocoder, map, infowindow) {
+    const input = document.getElementById("latlng").value;
+    const latlngStr = input.split(",", 2);
+    const latlng = {
+      lat: parseFloat(latlngStr[0]),
+      lng: parseFloat(latlngStr[1]),
+    };
+  
+    geocoder
+      .geocode({ location: latlng })
+      .then((response) => {
+        if (response.results[0]) {
+          map.setZoom(11);
+  
+          const marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+          });
+  
+          infowindow.setContent(response.results[0].formatted_address);
+          infowindow.open(map, marker);
+        } else {
+          window.alert("No results found");
+        }
+      })
+      .catch((e) => window.alert("Geocoder failed due to: " + e));
+  }
+  
+  window.initMap = initMap;
+
+
+
+  
